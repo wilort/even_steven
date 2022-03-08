407e04424e950b33d9d76e99a8f03e6a5f9e05b4
William Orton <william.orton.sorensen@gmail.com>
2022-03-08 23:12:39 +0100

    Clean up main function

--- main.cpp
+++ main.cpp
@@ -10,19 +10,20 @@ int main(int argc, char **argv) {
     std::string numbersFile;
     // "c:n:" indicates that we have flags -c and -n that
     // will be followed by an arguments which can be read as optarg
-    while ( (c = getopt (argc, argv, "c:n:")) != -1 )
-    switch (c) {
-      case 'c':
-        costsFile = optarg;
-        break;
-      case 'n':
-        numbersFile = optarg;
-        break;
-      default:
-        abort ();
+    while ( (c = getopt (argc, argv, "c:n:")) != -1 ){
+      switch (c) {
+        case 'c':
+          costsFile = optarg;
+          break;
+        case 'n':
+          numbersFile = optarg;
+          break;
+        default:
+          abort ();
       }
-    Problem problem;
+    }
 
+    Problem problem;
     problem.readCosts(costsFile);
     problem.readNumbers(numbersFile);
     problem.solve();
